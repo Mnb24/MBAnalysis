4242e0613bd47898e9bc1423499daf7276cfc4c8
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-04 12:19:00 +0530

    Update Summarizer.py

--- Summarizer.py
+++ Summarizer.py
@@ -87,7 +87,7 @@ def generate_summary(file_content, section_number, word_limit=200, top_n=5):
     return summary
 
 # Streamlit UI
-st.title('Document Viewer - Adi Parva')
+st.title('Document Summarizer - Adi Parva')
 
 # File paths
 file_paths = ['https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BD1.txt', 
