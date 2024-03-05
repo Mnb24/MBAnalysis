76d86fd8991d4663ac6db8810260e54e824796d8
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-05 10:31:41 +0530

    Update Summarizer2.py

--- Summarizer2.py
+++ Summarizer2.py
@@ -73,7 +73,7 @@ def textrank_summary(sentences, top_n=5):
     return summary
 
 # Streamlit UI
-st.title('Text Summarizer - TextRank Algorithm')
+st.title('Document Summarizer (TextRanker) - Adi Parva')
 
 # File paths
 file_paths = ['https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BD1.txt', 
