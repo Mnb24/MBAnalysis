d649b4fcb69a449d5c9595d6959ed3be5f30753c
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-07 10:40:05 +0530

    Update StringMatch2.py

--- StringMatch2.py
+++ StringMatch2.py
@@ -9,19 +9,14 @@ def perform_string_matching(files, target_word):
 
     occurrences = {file_name: 0 for file_name in files.keys()}  # Dictionary to store occurrences per file
 
-    # Iterate over each file
+    # Count occurrences in each file
     for file_name, file_path in files.items():
         response = requests.get(file_path)
         lines = response.text.split('\n')
 
-        st.write(f"Results from file: {file_name}")
-
         # Check each line for the target word
-        for line_number, line in enumerate(lines, start=1):
+        for line in lines:
             if target_word in line:
-                # Highlight the target word with a color
-                highlighted_line = line.replace(target_word, f"<span style='color: red'>{target_word}</span>")
-                st.write(f"Line {line_number}: {highlighted_line}", unsafe_allow_html=True)
                 occurrences[file_name] += 1
 
     # Create DataFrame for word distribution
@@ -30,6 +25,23 @@ def perform_string_matching(files, target_word):
     # Plot word distribution
     st.write("Word Distribution:")
     st.bar_chart(df.set_index('File'))
+
+    st.write("")  # Empty line for spacing
+
+    # Display results for each file
+    st.write("Results:")
+    for file_name, file_path in files.items():
+        response = requests.get(file_path)
+        lines = response.text.split('\n')
+
+        st.write(f"Results from file: {file_name}")
+        
+        # Check each line for the target word
+        for line_number, line in enumerate(lines, start=1):
+            if target_word in line:
+                # Highlight the target word with a color
+                highlighted_line = line.replace(target_word, f"<span style='color: red'>{target_word}</span>")
+                st.write(f"Line {line_number}: {highlighted_line}", unsafe_allow_html=True)
 
 def main():
     # Displaying heading
