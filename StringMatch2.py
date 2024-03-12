25d468835b71072cb8698628f2a94242d9ec75c1
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-12 09:50:47 +0530

    Update StringMatch2.py

--- StringMatch2.py
+++ StringMatch2.py
@@ -45,7 +45,7 @@ def perform_string_matching(files, target_word):
 
 def main():
     # Displaying heading
-    st.title("String Matcher - Adi Parva")
+    st.title("String Match Tool- Adi Parva")
 
     files = {
         'BR': 'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BR-01-Complete.txt',
