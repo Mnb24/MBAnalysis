9f6ee8c0b9d97f820de86759d0720e47b179c630
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-14 11:54:00 +0530

    Create WordCloud.py

---
+++ WordCloud.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from wordcloud import WordCloud
+from collections import Counter
+import re
+import requests
+
+# Function to count words in text
+def count_words_in_text(text):
+    words_text = re.findall(r'\w+', text.lower())
+    return Counter(words_text)
+
+st.title('Word Cloud Generator')
+
+# File URLs
+file_urls = {
+    'Bibek Debroy': 'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BD1.txt',
+    'KM Ganguly': 'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/KMG1.txt',
+    'MN Dutt': 'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/MND1.txt'
+}
+
+# Translation selection
+selected_translation = st.selectbox("Select translation:", list(file_urls.keys()))
+
+# Get text from selected translation URL
+response = requests.get(file_urls[selected_translation])
+text = response.text
+
+if st.button('Generate Word Cloud'):
+    # Count words in the text
+    word_counts = count_words_in_text(text)
+    
+    # Generate word cloud
+    wordcloud = WordCloud().generate_from_frequencies(word_counts)
+    
+    # Display word cloud
+    st.image(wordcloud.to_array())
