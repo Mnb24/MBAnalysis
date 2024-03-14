72304379cbfc6132be221afbf2aa02b7ad778757
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-14 11:09:56 +0530

    Update Plots.py

--- Plots.py
+++ Plots.py
@@ -64,7 +64,7 @@ if st.button('Analyze'):
     # Create a bar plot for the top words
     plt.figure(figsize=(10, 6))
     sns.barplot(x='Word', y='Frequency', data=df)
-    plt.title('Top 10 Words in Section {}\n\n\n\n'.format(section_number))
+    plt.title('Top 10 Words in Section {}\n\n\n\n\n\n\n\n\n\n\n\n'.format(section_number))
     plt.xticks(rotation=45, fontsize=12)  # Increase font size
     plt.yticks(fontsize=12)  # Increase font size
     plt.xlabel('Word', fontsize=14)  # Increase font size
