31379dd0964c6491fc38501ff04c4a87c5641a15
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-20 11:38:48 +0530

    Update ParvaViewer.py

--- ParvaViewer.py
+++ ParvaViewer.py
@@ -14,8 +14,8 @@ def get_parva(file_content, parva_number):
             elif found_parva:
                 break
         elif found_parva:
-            if line.strip().startswith("BR-"):  # Check if line starts with "BR-"
-                parva_content.append('\n' + line)  # Add newline before lines starting with "BR-"
+            if line.strip().startswith("BR-") or line.strip().startswith("KK-"):  # Check if line starts with "BR-" or "KK-"
+                parva_content.append('\n' + line)  # Add newline before lines starting with "BR-" or "KK-"
             else:
                 parva_content.append(line)
     return '\n'.join(parva_content)
@@ -23,21 +23,29 @@ def get_parva(file_content, parva_number):
 # Streamlit UI
 st.title('Mahabharata Parva Viewer')
 
-# File path
-file_path = 'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BR-Complete.txt'
+# File paths
+file_paths = {
+    "BORI (BR)": 'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BR-Complete.txt',
+    "Kumbakonam (KK)": 'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/KK-Complete.txt'
+}
 
 # Parva names
 parva_names = ["Adi Parva", "Sabha Parva", "Vana Parva", "Virata Parva", "Udyoga Parva", "Bhishma Parva", "Drona Parva",
                "Karna Parva", "Shalya Parva", "Sauptika Parva", "Stri Parva", "Shanti Parva", "Anushasana Parva", 
                "Ashvamedhika Parva", "Ashramavasika Parva", "Mausala Parva", "Mahaprasthanika Parva", "Svargarohanika Parva"]
 
+# Allow user to select translation
+selected_translation = st.selectbox('Select Translation:', list(file_paths.keys()))
+
 # Allow user to select parva
 selected_parva = st.selectbox('Select the Parva:', parva_names)
 
 if st.button('View Parva'):
+    file_path = file_paths[selected_translation]
     response = requests.get(file_path)
     file_content = response.text
     parva_number = parva_names.index(selected_parva) + 1  # Parva numbers start from 1
     parva_content = get_parva(file_content, parva_number)
-    st.markdown(f"## {selected_parva}:")
+    st.markdown(f"## {selected_translation} - {selected_parva}:")
     st.write(parva_content)
+
