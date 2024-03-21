dc5b740deb7b5510319e76b8be71578cf148784a
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-21 11:04:13 +0530

    Update ParvaViewer.py

--- ParvaViewer.py
+++ ParvaViewer.py
@@ -3,6 +3,11 @@ import requests
 import zipfile
 import io
 
+# Parva names
+parva_names = ["Adi Parva", "Sabha Parva", "Vana Parva", "Virata Parva", "Udyoga Parva", "Bhishma Parva", "Drona Parva",
+               "Karna Parva", "Shalya Parva", "Sauptika Parva", "Stri Parva", "Shanti Parva", "Anushasana Parva", 
+               "Ashvamedhika Parva", "Ashramavasika Parva", "Mausala Parva", "Mahaprasthanika Parva", "Svargarohanika Parva"]
+
 def get_parva(file_content, parva_number):
     parvas = file_content.split('\n')
     found_parva = False
@@ -58,3 +63,4 @@ if st.button('View Parva'):
     parva_content = get_parva(file_content, parva_number)
     st.markdown(f"## {selected_translation} - {selected_parva}:")
     st.write(parva_content)
+
