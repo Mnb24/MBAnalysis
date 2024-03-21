175aa841bedce785d37ce43b3670d4bd2b4dd9d6
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-03-21 11:15:30 +0530

    Update ParvaViewer.py

--- ParvaViewer.py
+++ ParvaViewer.py
@@ -3,11 +3,6 @@ import requests
 import zipfile
 import io
 
-# Parva names
-parva_names = ["Adi Parva", "Sabha Parva", "Vana Parva", "Virata Parva", "Udyoga Parva", "Bhishma Parva", "Drona Parva",
-               "Karna Parva", "Shalya Parva", "Sauptika Parva", "Stri Parva", "Shanti Parva", "Anushasana Parva", 
-               "Ashvamedhika Parva", "Ashramavasika Parva", "Mausala Parva", "Mahaprasthanika Parva", "Svargarohanika Parva"]
-
 def get_parva(file_content, parva_number):
     parvas = file_content.split('\n')
     found_parva = False
@@ -45,16 +40,15 @@ selected_parva = st.selectbox('Select the Parva:', parva_names)
 if st.button('View Parva'):
     file_path = file_paths[selected_translation]
     if file_path.endswith(".zip"):
-        response = requests.get(file_path)
+        response = requests.get(file_path, stream=True)
         with zipfile.ZipFile(io.BytesIO(response.content)) as z:
             file_list = z.namelist()
-            print("File list in ZIP archive:", file_list)  # Print list of files in ZIP archive
             for filename in file_list:
                 if filename.endswith(".txt"):
-                    print("Reading file:", filename)  # Print the name of the file being read
                     with z.open(filename) as f:
-                        file_content = f.read().decode('utf-8')
-                        print("Content length:", len(file_content))  # Print length of the file content
+                        file_content = ""
+                        for chunk in f:
+                            file_content += chunk.decode('utf-8')
                         break  # Stop after reading the first TXT file
     else:
         response = requests.get(file_path)
@@ -64,4 +58,3 @@ if st.button('View Parva'):
     parva_content = get_parva(file_content, parva_number)
     st.markdown(f"## {selected_translation} - {selected_parva}:")
     st.write(parva_content)
-
