4a3e1341da38c397ad10258a6e62637fb8f88884
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-04-02 10:47:14 +0530

    Update Concordance.py

--- Concordance.py
+++ Concordance.py
@@ -1,10 +1,26 @@
 import streamlit as st
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.text import Text
 import requests
 import nltk
 
 # Download nltk resources
 nltk.download('punkt')
+
+def get_context_paragraphs(text, target_word, context_lines=2):
+    sentences = sent_tokenize(text)
+    paragraphs = []
+
+    # Find sentences containing the target word
+    for i, sentence in enumerate(sentences):
+        if target_word in word_tokenize(sentence):
+            start_index = max(0, i - context_lines)
+            end_index = min(len(sentences), i + context_lines + 1)
+            context_sentences = sentences[start_index:end_index]
+            context_paragraph = " ".join(context_sentences)
+            paragraphs.append(context_paragraph)
+
+    return paragraphs
 
 def perform_concordance(texts, target_word):
     # Initialize concordance lists for each text
@@ -38,25 +54,23 @@ def perform_concordance(texts, target_word):
             st.write("\n\n")
 
 def main():
-    # Displaying heading
     st.title("Concordance Analyzer - Adi Parva")
-
     # URLs of the text files
     file_paths = [
         'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/BD1.txt', 
         'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/KMG1.txt', 
         'https://raw.githubusercontent.com/Mnb24/MBAnalysis/main/MND1.txt'
     ]
-    text_names = ['BD1', 'KMG1', 'MND1']
+
+    texts = []
+    for file_path in file_paths:
+        response = requests.get(file_path)
+        texts.append(response.text)
 
     target_word = st.text_input("Enter the word for concordance analysis: ")
 
     if st.button('Perform Concordance Analysis'):
-        for file_path, text_name in zip(file_paths, text_names):
-            response = requests.get(file_path)
-            text = response.text
-            st.markdown(f"<h2 style='font-size:24px'>{text_name}</h2>", unsafe_allow_html=True)
-            perform_concordance(text, target_word, text_name)
+        perform_concordance(texts, target_word)
 
 if __name__ == "__main__":
     main()
