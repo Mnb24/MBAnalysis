4f670da3fcd9e63a91001f2d235081833e232408
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-04-03 11:33:41 +0530

    Update Compare2.py

--- Compare2.py
+++ Compare2.py
@@ -7,30 +7,24 @@ def print_colored_diff(sentence1, sentence2):
     colored_sentence1 = ''
     colored_sentence2 = ''
 
-    differ = difflib.Differ()
-    diff = list(differ.compare(sentence1, sentence2))
-
-    for item in diff:
-        code = item[:1]
-        word = item[2:]
-        if code == ' ':
-            colored_sentence1 += word
-            colored_sentence2 += word
-        elif code == '+':
-            colored_sentence1 += f'<span style="color: blue">{word}</span>'
-        elif code == '-':
-            colored_sentence2 += f'<span style="color: red">{word}</span>'
+    for char1, char2 in zip(sentence1, sentence2):
+        if char1 == char2:
+            colored_sentence1 += char1
+            colored_sentence2 += char2
+        else:
+            colored_sentence1 += f'<span style="color: blue">{char1}</span>'
+            colored_sentence2 += f'<span style="color: red">{char2}</span>'
 
     return colored_sentence1, colored_sentence2
 
-# Function to find text differences sentence by sentence
+# Function to find text differences character by character
 def find_text_differences(text1, text2):
     differences = []
 
     differ = difflib.Differ()
 
     # Print the formatted differences with context
-    diff = list(differ.compare(text1.splitlines(keepends=True), text2.splitlines(keepends=True)))
+    diff = differ.compare(text1, text2)
     formatted_diff = [(code, word) for item in diff for code, word in [(item[:1], item[2:])]]
 
     differences.append(print_colored_diff(text1, text2))
@@ -63,32 +57,30 @@ if compare_button:
         responses = [requests.get(file_path) for file_path in file_paths]
         texts = [response.text for response in responses]
 
+        # Get the number of characters in the shortest file
+        min_chars = min(len(text) for text in texts)
+
         # Compare characters from each pair of files
-        differences_12 = find_text_differences(texts[0], texts[1])
-        differences_23 = find_text_differences(texts[1], texts[2])
-        differences_13 = find_text_differences(texts[0], texts[2])
+        for char_index in range(min_chars):
+            differences_12 = find_text_differences(texts[0][char_index], texts[1][char_index])
+            differences_23 = find_text_differences(texts[1][char_index], texts[2][char_index])
+            differences_13 = find_text_differences(texts[0][char_index], texts[2][char_index])
 
-        # Print differences for each pair of files
-        if differences_12:
-            sentence1, sentence2 = differences_12[0]
-            st.markdown("File1 - BORI")
-            st.markdown(sentence1, unsafe_allow_html=True)
-            st.markdown("File2 - Kumbakonam")
-            st.markdown(sentence2, unsafe_allow_html=True)
+            # Print differences for each pair of files
+            if differences_12:
+                sentence1, sentence2 = differences_12[0]
+                st.markdown(f"File1 - BORI: {sentence1}", unsafe_allow_html=True)
+                st.markdown(f"File2 - Kumbakonam: {sentence2}", unsafe_allow_html=True)
 
-        if differences_23:
-            sentence1, sentence2 = differences_23[0]
-            st.markdown("File2 - Kumbakonam")
-            st.markdown(sentence1, unsafe_allow_html=True)
-            st.markdown("File3 - Sastri Vavilla")
-            st.markdown(sentence2, unsafe_allow_html=True)
+            if differences_23:
+                sentence1, sentence2 = differences_23[0]
+                st.markdown(f"File2 - Kumbakonam: {sentence1}", unsafe_allow_html=True)
+                st.markdown(f"File3 - Sastri Vavilla: {sentence2}", unsafe_allow_html=True)
 
-        if differences_13:
-            sentence1, sentence2 = differences_13[0]
-            st.markdown("File1 - BORI")
-            st.markdown(sentence1, unsafe_allow_html=True)
-            st.markdown("File3 - Sastri Vavilla")
-            st.markdown(sentence2, unsafe_allow_html=True)
+            if differences_13:
+                sentence1, sentence2 = differences_13[0]
+                st.markdown(f"File1 - BORI: {sentence1}", unsafe_allow_html=True)
+                st.markdown(f"File3 - Sastri Vavilla: {sentence2}", unsafe_allow_html=True)
 
     except Exception as e:
         st.write(f"An error occurred: {str(e)}")
@@ -99,32 +91,30 @@ if compare_gita_button:
         responses = [requests.get(file_path) for file_path in bhagavad_gita_file_paths]
         texts = [response.text for response in responses]
 
+        # Get the number of characters in the shortest file
+        min_chars = min(len(text) for text in texts)
+
         # Compare characters from each pair of files
-        differences_12 = find_text_differences(texts[0], texts[1])
-        differences_23 = find_text_differences(texts[1], texts[2])
-        differences_13 = find_text_differences(texts[0], texts[2])
+        for char_index in range(min_chars):
+            differences_12 = find_text_differences(texts[0][char_index], texts[1][char_index])
+            differences_23 = find_text_differences(texts[1][char_index], texts[2][char_index])
+            differences_13 = find_text_differences(texts[0][char_index], texts[2][char_index])
 
-        # Print differences for each pair of files
-        if differences_12:
-            sentence1, sentence2 = differences_12[0]
-            st.markdown("File1 - BORI")
-            st.markdown(sentence1, unsafe_allow_html=True)
-            st.markdown("File2 - Kumbakonam")
-            st.markdown(sentence2, unsafe_allow_html=True)
+            # Print differences for each pair of files
+            if differences_12:
+                sentence1, sentence2 = differences_12[0]
+                st.markdown(f"File1 - BORI: {sentence1}", unsafe_allow_html=True)
+                st.markdown(f"File2 - Kumbakonam: {sentence2}", unsafe_allow_html=True)
 
-        if differences_23:
-            sentence1, sentence2 = differences_23[0]
-            st.markdown("File2 - Kumbakonam")
-            st.markdown(sentence1, unsafe_allow_html=True)
-            st.markdown("File3 - Sastri Vavilla")
-            st.markdown(sentence2, unsafe_allow_html=True)
+            if differences_23:
+                sentence1, sentence2 = differences_23[0]
+                st.markdown(f"File2 - Kumbakonam: {sentence1}", unsafe_allow_html=True)
+                st.markdown(f"File3 - Sastri Vavilla: {sentence2}", unsafe_allow_html=True)
 
-        if differences_13:
-            sentence1, sentence2 = differences_13[0]
-            st.markdown("File1 - BORI")
-            st.markdown(sentence1, unsafe_allow_html=True)
-            st.markdown("File3 - Sastri Vavilla")
-            st.markdown(sentence2, unsafe_allow_html=True)
+            if differences_13:
+                sentence1, sentence2 = differences_13[0]
+                st.markdown(f"File1 - BORI: {sentence1}", unsafe_allow_html=True)
+                st.markdown(f"File3 - Sastri Vavilla: {sentence2}", unsafe_allow_html=True)
 
     except Exception as e:
         st.write(f"An error occurred: {str(e)}")
