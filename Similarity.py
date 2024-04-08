f4695a0f5600e6a5584b55a23d78bc26443b11cd
Mnb24 <159748157+Mnb24@users.noreply.github.com>
2024-04-08 11:49:33 +0530

    Update Similarity.py

--- Similarity.py
+++ Similarity.py
@@ -37,7 +37,7 @@ def main():
 
     # Sidebar with text boxes
     st.sidebar.header("Text from File 1 (MBTN.txt)")
-    st.sidebar.write(mbtn_text)
+    st.sidebar.text_area("Selected Text", mbtn_text, height=400)
 
     target_words = st.sidebar.text_area("Enter words to find matches", height=200).split()
 
